006b89b2c3e01c4f8284f180340b81aad5cb470d
shr-NaHCO3 <shrshr2@163.com>
2022-08-25 18:02:26 +0800

    commit updaate

--- doc/temp/temp.js
+++ doc/temp/temp.js
@@ -1,20 +1,48 @@
-new Promise((resolve, reject)=>{
-  console.log(`${'\x1B[32m'}hello world!${'\x1B[0m'}`);
-  setTimeout(() => {
-    console.log(`time: ${new Date().getTime()}`);
-    resolve({}+[]);
-  }, 1000);
-})
-.then(value => new Promise((resolve, reject)=>{
-  if(typeof(value) === void 0){
-    resolve(114514);
-  }else{
-    reject(1919810);
+// new Promise((resolve, reject)=>{
+//   console.log(`${'\x1B[32m'}hello world!${'\x1B[0m'}`);
+//   setTimeout(() => {
+//     console.log(`time: ${new Date().getTime()}`);
+//     resolve({}+[]);
+//   }, 1000);
+// })
+// .then(value => new Promise((resolve, reject)=>{
+//   if(typeof(value) === void 0){
+//     resolve(114514);
+//   }else{
+//     reject(1919810);
+//   }
+// }))
+// .then(value=>{
+//   console.log(value);
+// })
+// .catch(error=>{
+//   console.log(error);
+// })
+
+
+class Yangzheng_Junior_Motherfucking_High_School {
+  constructor($){
+    'use strict'
+    this.type = `RUBBISH`;
+    this.description = `a motherf■■king FORMALISTIC unbitchment insane unbelievable inhumane school`;
   }
-}))
-.then(value=>{
-  console.log(value);
-})
-.catch(error=>{
-  console.log(error);
-})
+
+  join($){
+    console.log('welcome to our school!')
+    new Promise((res, rej)=>{
+      setTimeout(()=>{
+        console.log(`it is ${this.description}`)
+        res(114514)
+      },1000)      
+    })
+    .then(val => new Promise((res, rej) => {
+      setTimeout(()=>{
+        console.log(`the description is:`)
+        res(1919810)
+      },1000)      
+    }))
+    .then(val => {
+      throw "找不到讲述者"
+    })
+  }
+}
